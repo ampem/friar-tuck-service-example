6a0044f27863a23cd3c776cc61948a3b77e38b23
Ampem Darko <ampem.darko@gmail.com>
2025-08-22 11:23:16 +0100

    Remove duplicate applications

---
+++ app/handler_http.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, Response, status
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "OK"}
+
+@app.get("/fail")
+def fail(response: Response):
+    response.status_code = status.HTTP_400_BAD_REQUEST
+    return {"message": "Failure"}
